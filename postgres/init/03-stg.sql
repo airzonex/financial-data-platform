e49b2aff297cc8d5e54312d6457b8960b2dafd05
Airat Khaziev <air.zone.startup@gmail.com>
2026-09-25 16:41:25 +0400

    Fix postgres/init/03-stg.sql

--- postgres/init/03-stg.sql
+++ postgres/init/03-stg.sql
@@ -1,11 +1,11 @@
-create table stg.rgbi_history(
+create table if not exists stg.rgbi_history(
 	run_id bigint not null,
 	trade_date text,
 	close text,
 	currency_id text
 );
 
-create table stg.keyrate_history(
+create table if not exists stg.keyrate_history(
 	run_id bigint not null,
 	trade_date text,
 	keyrate text
